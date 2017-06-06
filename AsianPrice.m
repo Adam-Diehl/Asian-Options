c701fee45ff8bb3063dd1b07baf1cc41584a6566
Adam-Diehl <adamtdiehl1996@gmail.com>
2017-06-06 15:19:42 -0400

    Basic Asian Option Price (Matlab)
    (Works identically to the similarly titled R program)
    
    Calculates the Asian call and put prices using Monte Carlo simulation, assuming a European style exercise and geometric Brownian motion.

---
+++ AsianPrice.m
@@ -0,0 +1,38 @@
+%Asian Option Pricing
+
+function [AsianCall,AsianPut]=AsianPrice(S0,K,r,vol,T,dt,Nrows)
+%Set Constants
+tic
+    
+%Pre-calculate as many parameters as possible
+    Ncols = T/dt;
+    drift = (r-((vol^2)/2))*dt; 
+    part_stoch_part = vol*sqrt(dt); 
+    Discount = exp(-r*T);
+
+%Generate Random numbers
+    random = randn(Nrows, Ncols);
+
+%Generate Stock Price Paths
+    S = ones(Nrows, Ncols);
+    S = S.*exp(drift+part_stoch_part.*random);
+    S = cumprod(S,2);
+    S = S0*S;
+    S(:,1) = S0;
+
+clear random; %Free up a chunk of RAM
+
+%Calculate Asian Option Prices
+    %Asian Call
+        %Path Averages
+            A_Path = (1/Ncols)*sum(S,2);
+        %Calculate Payoff
+            Payoff_Call = [zeros(Nrows,1) A_Path - K];
+            Payoff_Call = Discount.*max(Payoff_Call,[],2);
+            AsianCall = mean(Payoff_Call);
+    %Asian Put
+        %Calculate Payoff
+            Payoff_Put = [zeros(Nrows,1) K - A_Path];
+            Payoff_Put = exp(-r*T).*max(Payoff_Put,[],2);
+            AsianPut = mean(Payoff_Put);
+toc
